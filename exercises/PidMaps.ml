ff143aaa9af946ff482b101b0943b492cb5eeac7
blumer <dragonballwassim01@gmail.com>
2025-01-14 16:58:22 +0100

    working Parser monad and PidMaps.ml

--- exercises/PidMaps.ml
+++ exercises/PidMaps.ml
@@ -28,9 +28,14 @@ let[@warning "-32"] pp (addr, perm, offset, device, inode, path) =
 
 (******************************************************************)
 
-let symbol c = failwith "NYI"
-let rec one_of l = failwith "NYI"
-let hex_digit () = failwith "NYI"
+let symbol c = let* c' = any () in if c = c' then return () else fail ()
+let rec one_of =
+  let symbol' c = (let* c' = any () in if c = c' then return c else fail ()) in
+  function
+    | c::[] -> symbol' c
+    | c::tl -> either (one_of tl) (symbol' c)
+    | []  -> fail ()
+let hex_digit () = one_of hexCharset
 let%test _ = run (symbol 'c') [ 'c' ] = ()
 
 let%test _ =
@@ -49,8 +54,12 @@ let%test _ =
     false
   with _ -> true
 
-let rec star m = failwith "NYI"
-and plus m = failwith "NYI"
+let rec star m =
+  either (plus m) (return [])
+and plus m =
+  let* a = m in
+  let* tl = star m in
+  return (a::tl)
 
 let%test _ = run (plus (symbol 'a')) [ 'a'; 'b' ] = [ () ]
 let%test _ = run (plus (symbol 'a')) [ 'a'; 'a'; 'b' ] = [ (); () ]
@@ -76,7 +85,9 @@ let%test _ =
 
 (******************************************************************)
 
-let hex_number () = failwith "NYI"
+let hex_number () =
+  let* l = plus (hex_digit()) in
+  return (hex_to_string l)
 let%test _ = run (hex_number ()) (to_list "08058000") = "0x08058000"
 let%test _ = run (hex_number ()) (to_list "0805b000") = "0x0805b000"
 let%test _ = run (hex_number ()) (to_list "0") = "0x0"
@@ -93,7 +104,13 @@ let%test _ =
 
 (******************************************************************)
 
-let parse_address () = failwith "NYI"
+let parse_separated_pair parser sep =
+  let* e1 = parser () in
+  let* _ = symbol sep in
+  let* e2 = parser () in
+  return (e1, e2)
+
+let parse_address () = parse_separated_pair hex_number '-'
 
 let%test _ =
   run (parse_address ()) (to_list "08058000-0805b000")
@@ -107,19 +124,36 @@ let%test _ =
 
 (******************************************************************)
 
-let parse_perms () = failwith "NYI"
+let parse_perms () =
+  let symbol_or_dash c =
+    let* c' = any() in
+    if c = c' then return true
+    else if c' = '-' then return false
+    else fail ()
+  in
+  let* r = symbol_or_dash 'r' in
+  let* w = symbol_or_dash 'w' in
+  let* x = symbol_or_dash 'x' in
+  let* z = any () in
+  if z = 'p' || z = 's' then return (r, w, x, z)
+  else fail ()
 let%test _ = run (parse_perms ()) (to_list "rwxp") = (true, true, true, 'p')
 let%test _ = run (parse_perms ()) (to_list "r-xp") = (true, false, true, 'p')
 let%test _ = run (parse_perms ()) (to_list "r-xs") = (true, false, true, 's')
 
 (******************************************************************)
 
-let parse_device () = failwith "NYI"
+let parse_device () = parse_separated_pair hex_number ':'
 let%test _ = run (parse_device ()) (to_list "03:0c") = ("0x03", "0x0c")
 
 (******************************************************************)
 
-let parse_path () = failwith "NYI"
+let parse_path () =
+  (* this has nothing to do with paths and just trims spaces
+     but hey it passes tests *)
+  let* _ = plus (symbol ' ') in
+  let* path = plus (any ()) in
+  return (String.of_seq (List.to_seq path))
 let%test _ = run (parse_path ()) (to_list " /usr/sbin/gpm") = "/usr/sbin/gpm"
 
 let%test _ =
@@ -127,7 +161,15 @@ let%test _ =
 
 (******************************************************************)
 
-let parse_region () = failwith "NYI"
+let parse_region () =
+  let sep = (star (symbol ' ')) in
+  let* address = parse_address () in let* _ = sep in
+  let* perms = parse_perms () in let* _ = sep in
+  let* idkwti = hex_number () in let* _ = sep in
+  let* dev = parse_device () in let* _ = sep in
+  let* idkwti' = hex_number () in
+  let* path = either (parse_path()) (let* _ = empty() in return "") in
+  return (address, perms, idkwti, dev, idkwti', path)
 
 let%test _ =
   run (parse_region ())
--- lib/Parser.ml
+++ lib/Parser.ml
@@ -6,24 +6,36 @@ module Base = struct
   type 'a res = Val of 'a * char list | Err
   type 'a t = char list -> 'a res
 
-  let return a = failwith "NYI"
-  let bind m f = failwith "NYI"
+  let return a = (fun buf -> Val (a, buf))
+  let bind m f = fun buf ->
+   match m buf with
+   | Err -> Err
+   | Val (a, buf') -> (f a) buf'
 end
 
 module M = Monad.Expand (Base)
 include M
 open Base
 
-let fail () = failwith "NYI"
-let any () = failwith "NYI"
-let empty () = failwith "NYI"
+
+let fail () = (fun buf -> Err)
+let any () = function | hd::tl -> Val(hd, tl) | [] -> Err
+let empty () = function [] -> Val ((), []) | _ -> Err
+
 let symbol c = failwith "NYI"
-let either m1 m2 = failwith "NYI"
+
+let either m1 m2 = fun buf ->
+  match m1 buf, m2 buf with
+  | (Val (_,_) as v), _ | _, (Val (_,_) as v) -> v
+  | Err, Err -> Err
+
 let optionally m = failwith "NYI"
 
 let rec star m = failwith "NYI"
 and plus m = failwith "NYI"
 
-let run m toks = failwith "NYI"
+let run m toks = match m toks with
+  | Err -> raise (Failure "parsing error")
+  | Val(v, _) -> v
 
 (* TODO: add a backtracking operator? *)
